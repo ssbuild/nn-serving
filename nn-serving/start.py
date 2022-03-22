6c8f467f64101df57581f5b6efa2db01322575af
王亚伟 <wangyawei@dhcc.com.cn>
2022-03-22 09:06:12 +0800

    2022年3月22日09:06:14
    Signed-off-by: 王亚伟 <wangyawei@dhcc.com.cn>

--- nn-serving/start.py
+++ nn-serving/start.py
@@ -11,11 +11,7 @@ from ipc_worker.ipc_zmq_loader import IPC_zmq,ZMQ_process_worker
 from  http_proxy import HTTP_Proxy
 
 from nn_sdk import csdk_wraper
-from bert_pretty import FullTokenizer
-from bert_pretty import text_feature_char_level_input_ids_mask
 from config.config import config as nn_config
-
-
 
 tmp_dir = './tmp'
 if not os.path.exists(tmp_dir):
@@ -28,21 +24,21 @@ class My_worker(ZMQ_process_worker):
         super(My_worker,self).__init__(*args,**kwargs)
         #config info , use by yourself
         self._logger.info('Process id {}, group name {} , identity {}'.format(self._idx,self._group_name,self._identity))
-
         config['model_config']['device_id'] = self._idx % device_num if device_num > 0 else 0
         self._logger.info(config)
-
-
-
         self.config = copy.deepcopy(config)
 
     #Process begin trigger this func
     def run_begin(self):
         self._logger.info('worker pid {}...'.format(os.getpid()))
+        from bert_pretty import FullTokenizer
+        from bert_pretty import text_feature_char_level_input_ids_mask
 
         self.tokenizer = FullTokenizer(self.config['vocab_file'],do_lower_case=True)
         self.sdk = csdk_wraper.csdk_object(self.config['model_config'])
         assert self.sdk.valid()
+
+        self.text_feature = text_feature_char_level_input_ids_mask
 
     # Process end trigger this func
     def run_end(self):
@@ -54,7 +50,7 @@ class My_worker(ZMQ_process_worker):
         texts = request_data.get('texts',[])
         param = request_data.get('param',{})
         max_len = self.config['max_len']
-        input_ids,input_mask = text_feature_char_level_input_ids_mask(self.tokenizer,texts,max_len=max_len,with_padding=False)
+        input_ids,input_mask = self.text_feature(self.tokenizer,texts,max_len=max_len,with_padding=False)
         code,preds = self.sdk.process(0,input_ids,input_mask)
         if code != 0:
             return [ -1 * len(texts) ]
